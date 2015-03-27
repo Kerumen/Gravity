2d2c4288f26b4b5b6d10a7c42c9aa1738d048ff7
Yann Pringault <ypringau@student.42.fr>
2015-03-27 16:07:11 +0100

    feat(home): add beginning of home

--- client/views/home/home.controller.js
+++ client/views/home/home.controller.js
@@ -5,8 +5,39 @@ angular.module('gravity')
 
     var vm = this;
 
-    angular.extend(vm, {
-      name: 'HomeCtrl'
-    });
+
+    TweenLite.defaultEase = Power0.easeNone;
+
+    var rocket = $('#layer-fusee');
+    var rocketContainer = $('#rocketContainer');
+
+    var t = new TimelineMax({ repeat: -1 });
+
+    t
+      .set(rocket, { x: 0, y: 300 })
+      .to(
+      rocket,
+      5,
+      {
+        bezier: { type:"thru", values: [
+          {x: 0, y: 300 },
+          {x: 350, y: 0 },
+          {x: 350, y: 70 },
+          {x: -20, y: 380 },
+          {x: 0, y: 300 }
+        ],
+          autoRotate: ["x","y","rotation",false]
+        }
+      }
+    );
+
+    var rocketCtAnim = new TimelineMax({ repeat: -1 });
+    rocketCtAnim
+      .set(rocketContainer, { zIndex: 3 })
+      .to(rocketContainer, 2.5, { y: 100 })
+      .set(rocketContainer, { zIndex: 1 })
+      .to(rocketContainer, 2.5, { y: 0 });
+
+
 
   });
