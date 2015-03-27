568690dc700f574029b170db59a87a13ae44af48
janteuni <janteuni@student.42.fr>
2015-03-27 20:00:27 +0100

    feat(tags): add tag container

--- client/views/profile/profile.controller.js
+++ client/views/profile/profile.controller.js
@@ -6,6 +6,27 @@ angular.module('gravity')
     var vm = this;
 
     angular.extend(vm, {
+      addTag: function(tag) {
+        vm.myTags.push(tag);
+      },
+      removeTag: function(index) {
+        vm.myTags.splice(index, 1);
+      },
+      myTags : [
+        { name: "Marketing", type: "tag-blue"}
+      ],
+      tags : [
+        { name: "Evian", type: "tag-blue"},
+        { name: "IT", type: "tag-red"},
+        { name: "France", type: "tag-green"},
+        { name: "Mobilité", type: "tag-red"},
+        { name: "Sales", type: "tag-red"},
+        { name: "Activia", type: "tag-blue"},
+        { name: "RSE", type: "tag-red"},
+        { name: "USA", type: "tag-green"},
+        { name: "Danette", type: "tag-green"},
+        { name: "Santé", type: "tag-blue"}
+      ]
     });
 
   });
