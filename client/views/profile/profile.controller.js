af7fc0e25134185d8a07ab0ae39599e6347602aa
Yann Pringault <ypringau@student.42.fr>
2015-03-27 23:27:29 +0100

    feat(profile): animate profile page

--- client/views/profile/profile.controller.js
+++ client/views/profile/profile.controller.js
@@ -5,9 +5,15 @@ angular.module('gravity')
 
     var vm = this;
 
+    vm.indexTagsTitle = 0;
+    vm.tagsTitle = ['Most used tags', 'Professional tags', 'Hobby tags'];
+    vm.customTag = null;
+
     angular.extend(vm, {
       addTag: function(tag) {
-        vm.myTags.push(tag);
+        var tagAdded = tag || { name: vm.customTag, type: 'tag-green' };
+        vm.myTags.push(tagAdded);
+        vm.customTag = null;
       },
       removeTag: function(index) {
         vm.myTags.splice(index, 1);
@@ -19,14 +25,53 @@ angular.module('gravity')
         { name: "Evian", type: "tag-blue"},
         { name: "IT", type: "tag-red"},
         { name: "France", type: "tag-green"},
-        { name: "Mobilité", type: "tag-red"},
+        { name: "Mobility", type: "tag-red"},
         { name: "Sales", type: "tag-red"},
         { name: "Activia", type: "tag-blue"},
-        { name: "RSE", type: "tag-red"},
+        { name: "Volvic", type: "tag-red"},
         { name: "USA", type: "tag-green"},
-        { name: "Danette", type: "tag-green"},
-        { name: "Santé", type: "tag-blue"}
+        { name: "Language", type: "tag-blue"},
+        { name: "Travel", type: "tag-red"},
+        { name: "Moto", type: "tag-green"},
+        { name: "Salut", type: "tag-green"},
+        { name: "Salut1", type: "tag-red"},
+        { name: "Salut2", type: "tag-green"},
+        { name: "Salut3", type: "tag-green"},
+        { name: "Salut4", type: "tag-red"},
+        { name: "Salut5", type: "tag-blue"},
+        { name: "Salut6", type: "tag-blue"},
+        { name: "Salut7", type: "tag-green"},
+        { name: "Salut8", type: "tag-red"},
+        { name: "Salut9", type: "tag-blue"},
+        { name: "Salut10", type: "tag-green"},
+        { name: "Salut11", type: "tag-green"},
+        { name: "Salut12", type: "tag-blue"},
+        { name: "Monocycle", type: "tag-red"}
       ]
     });
 
+    var content = $('#profile-view');
+    var t = new TimelineMax({delay: .5});
+
+    t
+      .set(content, {opacity: 0, y: 50})
+      .to(content, .5, {opacity: 1, y:0});
+
+    vm.scrollTagsLeft = function () {
+      vm.indexTagsTitle--;
+      var content = $('.tag-pool');
+      var t = new TimelineMax();
+
+      t
+        .to(content, .5, {x: "+=690"});
+    };
+
+    vm.scrollTagsRight = function () {
+      vm.indexTagsTitle++;
+      var content = $('.tag-pool');
+      var t = new TimelineMax();
+
+      t
+        .to(content, .5, {x: "-=690"});
+    };
   });
